197b4d78eb2500a3d294a738ecafbbdeeac32775
Ethan Bennis <ethan.bennis@gmail.com>
2021-09-20 21:22:39 -0700

    Add specular lighting model and clean up var names

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 // Standard lib
+#include <algorithm>
 #include <array>
 #include <cmath>
 #include <iostream>
@@ -14,10 +15,10 @@
 constexpr vec::Vec3f kOrigin{0.0F, 0.0F, 0.0F};
 
 constexpr int kCanvasWidth = 800U;
-constexpr int kCanvasHeight = 800U;
+constexpr int kCanvasHeight = 600U;
 
 constexpr float kViewportWidth = 1.0F;
-constexpr float kViewportHeight = 1.0F;
+constexpr float kViewportHeight = 0.75F;
 constexpr float kViewportDepth = 1.0F;
 
 /******************************************************************************
@@ -31,6 +32,7 @@ struct Sphere {
     vec::Vec3f center;
     float radius;
     sf::Color color;
+    float specularity;
 };
 
 // Scene spheres
@@ -39,16 +41,19 @@ const std::array<Sphere, 3> kSceneSpheres {{
         .center = vec::Vec3f{0.0F, -1.0F, 3.0F},
         .radius = 1.0F,
         .color = sf::Color::Red,
+        .specularity = 500,
     },
     {
         .center = vec::Vec3f{2.0F, 0.0F, 4.0F},
         .radius = 1.0F,
         .color = sf::Color::Blue,
+        .specularity = 500,
     },
     {
         .center = vec::Vec3f{-2.0F, 0.0F, 4.0F},
         .radius = 1.0F,
         .color = sf::Color::Green,
+        .specularity = 10,
     },
 }};
 
@@ -104,7 +109,7 @@ const std::array<Light, 3> kSceneLights {{
  * CANVAS
  ******************************************************************************/
 
-// Simple canvas abstraction to facilitate drawing individual pixels to the screen
+// Simple canvas abstraction to facilitate drawing individual pixels to the window
 class Canvas : public sf::Drawable {
 public:
     // Construct a canvas of specified dimensions and color (default black)
@@ -143,13 +148,15 @@ private:
  * RAYTRACING
  ******************************************************************************/
 
-// Scale the provided color struct (excluding alpha) by the provided intensity
+// Scale the provided color struct (excluding alpha) by the provided intensity, with saturation
 sf::Color scaleColor(const sf::Color& color, float intensity) {
-    return sf::Color(color.r * intensity, color.g * intensity, color.b * intensity, color.a);
+    return sf::Color(std::clamp(static_cast<int>(color.r * intensity), 0, 255),
+                     std::clamp(static_cast<int>(color.g * intensity), 0, 255),
+                     std::clamp(static_cast<int>(color.b * intensity), 0, 255));
 }
 
 // Compute basic lighting intensity at the specified point and surface normal
-float computeLighting(vec::Vec3f point, vec::Vec3f normal) {
+float computeLighting(vec::Vec3f point, vec::Vec3f normal, vec::Vec3f ray, float specularity) {
     float intensity = 0.0F;
 
     for (const Light& light : kSceneLights) {
@@ -162,12 +169,27 @@ float computeLighting(vec::Vec3f point, vec::Vec3f normal) {
                     light.direction :         // use direction directly
                     (light.position - point); // calculate direction from points
 
+            // Diffuse lighting
+            // TODO: have light intensity decrease following the inverse square law?
             const float normal_dot_direction = dot(normal, direction);
             // Check if light source is actually illuminating the point
             if (normal_dot_direction > 0.0F) {
                 // Calculate and add light intensity according to angle of incidence
                 intensity += (light.intensity * normal_dot_direction) /
                         (normal.euclidean() * direction.euclidean());
+            }
+
+            // Specular lighting
+            // Non-physical model: intensity is cos(alpha)^specularity, where alpha is the angle
+            // between the reflection vector and the negative of the ray vector we're tracing
+            if (specularity > 0.0F) {
+                vec::Vec3f reflection = 2.0F * normal * dot(normal, direction) - direction;
+                const float reflection_dot_ray = dot(reflection, -ray);
+                if (reflection_dot_ray > 0.0F) {
+                    const float reflection_ray_angle = reflection_dot_ray /
+                            (reflection.euclidean() * ray.euclidean());
+                    intensity += light.intensity * std::pow(reflection_ray_angle, specularity);
+                }
             }
         }
     }
@@ -177,23 +199,21 @@ float computeLighting(vec::Vec3f point, vec::Vec3f normal) {
 
 // Get sphere intersect points
 using RaySphereIntersect = std::pair<std::optional<float>, std::optional<float>>;
-RaySphereIntersect calcRaySphereIntersect(const vec::Vec3f& p0,
-                                          const vec::Vec3f& p1,
+RaySphereIntersect calcRaySphereIntersect(const vec::Vec3f& ray_point,
+                                          const vec::Vec3f& ray_vector,
                                           const Sphere& sphere) {
     const float r = sphere.radius;
-    const vec::Vec3f c_p0 = p0 - sphere.center;
+    const vec::Vec3f c_p = ray_point - sphere.center;
 
-    const float a = dot(p1, p1);
-    const float b = dot(c_p0, p1) * 2.0F;
-    const float c = dot(c_p0, c_p0) - (r*r);
+    const float a = dot(ray_vector, ray_vector);
+    const float b = dot(c_p, ray_vector) * 2.0F;
+    const float c = dot(c_p, c_p) - (r*r);
 
     const float discriminant = (b*b) - (4*a*c);
     if (discriminant < 0.0F) {
         // No intersect point
         return std::make_pair(std::nullopt, std::nullopt);
     }
-
-    // TODO: Is it worth trying to handle the discriminant ~= 0 case separately?
 
     const float disc_root = std::sqrt(discriminant);
     RaySphereIntersect t;
@@ -205,27 +225,30 @@ RaySphereIntersect calcRaySphereIntersect(const vec::Vec3f& p0,
 
 // Trace ray from first point toward second point over the provided independent
 // variable range. Return the color of the first object the ray collides with.
-sf::Color traceRay(const vec::Vec3f& p0, const vec::Vec3f& p1, float t_min, float t_max) {
+sf::Color traceRay(const vec::Vec3f& ray_point,
+                   const vec::Vec3f& ray_vector,
+                   float t_min,
+                   float t_max) {
     // Closest sphere intersect data
     float closest_t = std::numeric_limits<float>::infinity();
     std::optional<Sphere> closest_sphere;
 
     // TODO: find a better way to access scene data
     for (const Sphere& sphere : kSceneSpheres) {
-        // Get intersect point(s)
-        const RaySphereIntersect t = calcRaySphereIntersect(p0, p1, sphere);
+        // Get intersect point(s) if any
+        const RaySphereIntersect t = calcRaySphereIntersect(ray_point, ray_vector, sphere);
 
         // Check first potential intersect
-        if (t.first.has_value()) {
-            const float t0 = t.first.value();
+        if (t.first) {
+            const float t0 = *(t.first);
             if ((t0 > t_min) && (t0 < t_max) && t0 < closest_t) {
                 closest_t = t0;
                 closest_sphere = sphere;
             }
         }
         // Check second potential intersect
-        if (t.second.has_value()) {
-            const float t1 = t.second.value();
+        if (t.second) {
+            const float t1 = *(t.second);
             if ((t1 > t_min) && (t1 < t_max) && t1 < closest_t) {
                 closest_t = t1;
                 closest_sphere = sphere;
@@ -234,9 +257,12 @@ sf::Color traceRay(const vec::Vec3f& p0, const vec::Vec3f& p1, float t_min, floa
     }
 
     if (closest_sphere) {
-        const vec::Vec3f intersect_p = p0 + closest_t * (p1 - p0);
-        const vec::Vec3f normal = (intersect_p - closest_sphere.value().center).normalize();
-        return scaleColor(closest_sphere.value().color, computeLighting(intersect_p, normal));
+        const vec::Vec3f intersect_point = ray_point + closest_t * ray_vector;
+        const vec::Vec3f normal = (intersect_point - closest_sphere->center).normalize();
+        return scaleColor(closest_sphere->color, computeLighting(intersect_point,
+                                                                 normal,
+                                                                 ray_vector,
+                                                                 closest_sphere->specularity));
     } else {
         return sf::Color::White;
     }
