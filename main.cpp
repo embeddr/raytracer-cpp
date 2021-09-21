4ad412667363bf03c13052a39d582a49a9caeef8
Ethan Bennis <ethan.bennis@gmail.com>
2021-09-21 15:41:12 -0700

    Add shadows and perform cleanup

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ constexpr int kCanvasHeight = 600U;
 
 constexpr float kViewportWidth = 1.0F;
 constexpr float kViewportHeight = 0.75F;
-constexpr float kViewportDepth = 1.0F;
+constexpr float kViewportDepth = 0.75F;
 
 /******************************************************************************
  * SCENE TYPES AND DATA
@@ -36,7 +36,7 @@ struct Sphere {
 };
 
 // Scene spheres
-const std::array<Sphere, 3> kSceneSpheres {{
+const std::array<Sphere, 4> kSceneSpheres {{
     {
         .center = vec::Vec3f{0.0F, -1.0F, 3.0F},
         .radius = 1.0F,
@@ -54,6 +54,12 @@ const std::array<Sphere, 3> kSceneSpheres {{
         .radius = 1.0F,
         .color = sf::Color::Green,
         .specularity = 10,
+    },
+    {
+        .center = vec::Vec3f{0.0F, -5001.0F, 0.0F},
+        .radius = 5000.0F,
+        .color = sf::Color::Yellow,
+        .specularity = 1000,
     },
 }};
 
@@ -90,7 +96,7 @@ struct Light {
 
     static Light makeDirectional(float intensity, vec::Vec3f direction) {
         return Light {
-            .type = Type::kPoint,
+            .type = Type::kDirectional,
             .intensity = intensity,
             .position = {},
             .direction = direction,
@@ -155,50 +161,8 @@ sf::Color scaleColor(const sf::Color& color, float intensity) {
                      std::clamp(static_cast<int>(color.b * intensity), 0, 255));
 }
 
-// Compute basic lighting intensity at the specified point and surface normal
-float computeLighting(vec::Vec3f point, vec::Vec3f normal, vec::Vec3f ray, float specularity) {
-    float intensity = 0.0F;
-
-    for (const Light& light : kSceneLights) {
-        if (light.type == Light::Type::kAmbient) {
-            // Simply add ambient light intensity
-            intensity += light.intensity;
-        } else {
-            // Get light direction
-            const vec::Vec3f direction = (light.type == Light::Type::kDirectional) ?
-                    light.direction :         // use direction directly
-                    (light.position - point); // calculate direction from points
-
-            // Diffuse lighting
-            // TODO: have light intensity decrease following the inverse square law?
-            const float normal_dot_direction = dot(normal, direction);
-            // Check if light source is actually illuminating the point
-            if (normal_dot_direction > 0.0F) {
-                // Calculate and add light intensity according to angle of incidence
-                intensity += (light.intensity * normal_dot_direction) /
-                        (normal.euclidean() * direction.euclidean());
-            }
-
-            // Specular lighting
-            // Non-physical model: intensity is cos(alpha)^specularity, where alpha is the angle
-            // between the reflection vector and the negative of the ray vector we're tracing
-            if (specularity > 0.0F) {
-                vec::Vec3f reflection = 2.0F * normal * dot(normal, direction) - direction;
-                const float reflection_dot_ray = dot(reflection, -ray);
-                if (reflection_dot_ray > 0.0F) {
-                    const float reflection_ray_angle = reflection_dot_ray /
-                            (reflection.euclidean() * ray.euclidean());
-                    intensity += light.intensity * std::pow(reflection_ray_angle, specularity);
-                }
-            }
-        }
-    }
-
-    return intensity;
-}
-
 // Get sphere intersect points
-using RaySphereIntersect = std::pair<std::optional<float>, std::optional<float>>;
+using RaySphereIntersect = std::optional<std::pair<float, float>>;
 RaySphereIntersect calcRaySphereIntersect(const vec::Vec3f& ray_point,
                                           const vec::Vec3f& ray_vector,
                                           const Sphere& sphere) {
@@ -212,58 +176,124 @@ RaySphereIntersect calcRaySphereIntersect(const vec::Vec3f& ray_point,
     const float discriminant = (b*b) - (4*a*c);
     if (discriminant < 0.0F) {
         // No intersect point
-        return std::make_pair(std::nullopt, std::nullopt);
+        return std::nullopt;
     }
 
     const float disc_root = std::sqrt(discriminant);
-    RaySphereIntersect t;
-    t.first = (-b + disc_root) / (2*a);
-    t.second = (-b - disc_root) / (2*a);
 
-    return t;
+    return std::make_pair((-b + disc_root) / (2*a), (-b - disc_root) / (2*a));
 }
 
-// Trace ray from first point toward second point over the provided independent
-// variable range. Return the color of the first object the ray collides with.
-sf::Color traceRay(const vec::Vec3f& ray_point,
-                   const vec::Vec3f& ray_vector,
-                   float t_min,
-                   float t_max) {
+// Get the closest ray-sphere intersect, and a reference to the associated sphere
+struct ClosestRaySphereIntersect {
+    float t;
+    const Sphere& sphere;
+};
+std::optional<ClosestRaySphereIntersect> calcClosestRaySphereIntersect(const vec::Vec3f& point,
+                                                                       const vec::Vec3f& vector,
+                                                                       float t_min,
+                                                                       float t_max) {
     // Closest sphere intersect data
     float closest_t = std::numeric_limits<float>::infinity();
-    std::optional<Sphere> closest_sphere;
+    std::optional<std::reference_wrapper<const Sphere>> closest_sphere;
 
-    // TODO: find a better way to access scene data
     for (const Sphere& sphere : kSceneSpheres) {
-        // Get intersect point(s) if any
-        const RaySphereIntersect t = calcRaySphereIntersect(ray_point, ray_vector, sphere);
-
-        // Check first potential intersect
-        if (t.first) {
-            const float t0 = *(t.first);
-            if ((t0 > t_min) && (t0 < t_max) && t0 < closest_t) {
-                closest_t = t0;
+        // Get ray-sphere intersect points, if any
+        const RaySphereIntersect t = calcRaySphereIntersect(point, vector, sphere);
+        if (t) {
+            // Check ranges
+            if ((t->first > t_min) && (t->first < t_max) && t->first < closest_t) {
+                closest_t = t->first;
                 closest_sphere = sphere;
             }
-        }
-        // Check second potential intersect
-        if (t.second) {
-            const float t1 = *(t.second);
-            if ((t1 > t_min) && (t1 < t_max) && t1 < closest_t) {
-                closest_t = t1;
+            if ((t->second > t_min) && (t->second < t_max) && t->second < closest_t) {
+                closest_t = t->second;
                 closest_sphere = sphere;
             }
         }
     }
 
     if (closest_sphere) {
-        const vec::Vec3f intersect_point = ray_point + closest_t * ray_vector;
-        const vec::Vec3f normal = (intersect_point - closest_sphere->center).normalize();
-        return scaleColor(closest_sphere->color, computeLighting(intersect_point,
-                                                                 normal,
-                                                                 ray_vector,
-                                                                 closest_sphere->specularity));
+        return ClosestRaySphereIntersect{closest_t, (*closest_sphere).get()};
     } else {
+        return std::nullopt;
+    }
+}
+
+// Compute basic lighting intensity at the specified point and surface normal
+float computeLighting(vec::Vec3f point, vec::Vec3f normal, vec::Vec3f ray, float specularity) {
+    float intensity = 0.0F;
+
+    for (const Light& light : kSceneLights) {
+
+        if (light.type == Light::Type::kAmbient) {
+            // Simply add ambient light intensity
+            intensity += light.intensity;
+        } else {
+            // Get light direction and max occlusion t
+            vec::Vec3f direction;
+            float max_t_occlusion;
+
+            if (light.type == Light::Type::kDirectional) {
+                direction = light.direction;
+                max_t_occlusion = std::numeric_limits<float>::infinity();
+            } else {
+                direction = light.position - point;
+                max_t_occlusion = 1.0F;
+            }
+
+            // Check for clear line of sight to the light source
+            // TODO: Decrease light intensity with range? Inverse square law?
+            if (!calcClosestRaySphereIntersect(point, direction, 0.001F, max_t_occlusion)) {
+                // Diffuse lighting
+                const float normal_dot_direction = dot(normal, direction);
+                if (normal_dot_direction > 0.0F) {
+                    // Calculate and add light intensity according to angle of incidence
+                    intensity += light.intensity * normal_dot_direction /
+                                 (normal.euclidean() * direction.euclidean());
+                }
+
+                // Specular lighting
+                // Non-physical model: intensity is cos(alpha)^specularity, where alpha is the angle
+                // between the reflection vector and the negative of the ray vector we're tracing
+                if (specularity > 0.0F) {
+                    vec::Vec3f reflection = 2.0F * normal * dot(normal, direction) - direction;
+                    const float reflection_dot_ray = dot(reflection, -ray);
+                    if (reflection_dot_ray > 0.0F) {
+                        const float reflection_ray_angle = reflection_dot_ray /
+                                                           (reflection.euclidean() * ray.euclidean());
+                        intensity += light.intensity * std::pow(reflection_ray_angle, specularity);
+                    }
+                }
+            }
+        }
+    }
+
+    return intensity;
+}
+
+// Trace ray using provided point, vector, and independent variable range.
+// Calculate the color and lighting for the closest intersect point, if any.
+sf::Color traceRay(const vec::Vec3f& ray_point,
+                   const vec::Vec3f& ray_vector,
+                   float t_min,
+                   float t_max) {
+
+    // Get the closest sphere intersect, if any
+    const auto closest_intersect =
+            calcClosestRaySphereIntersect(ray_point, ray_vector, t_min, t_max);
+
+    if (closest_intersect) {
+        // Apply lighting intensity to sphere color at intersect
+        const vec::Vec3f intersect_point = ray_point + closest_intersect->t * ray_vector;
+        const vec::Vec3f normal = (intersect_point - closest_intersect->sphere.center).normalize();
+        const float intensity = computeLighting(intersect_point,
+                                                normal,
+                                                ray_vector,
+                                                closest_intersect->sphere.specularity);
+        return scaleColor(closest_intersect->sphere.color, intensity);
+    } else {
+        // Background color for no intersect
         return sf::Color::White;
     }
 }
